d759f3520e8885c30e50056ba6487ec046ffe359
Kostiantyn Korikov <lostclus@gmail.com>
2025-09-29 17:09:08 +0300

    Change version to 0.0.6

--- src/aiosafeconsumer/__init__.py
+++ src/aiosafeconsumer/__init__.py
@@ -10,7 +10,7 @@ from .types import DataType
 from .worker import Worker, WorkerSettings
 from .workerpool import WorkerDef, WorkerPool, WorkerPoolSettings
 
-__version__ = "0.0.5"
+__version__ = "0.0.6"
 
 __all__ = [
     "ConsumerWorker",
